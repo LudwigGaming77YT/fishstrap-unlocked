7c8add8040bef4f7de9285dda76bee8bae8eddb1
LudwigGaming77YT <43101660+LudwigGaming77YT@users.noreply.github.com>
2026-08-26 19:08:37 -0500

    Update BootstrapperIcon.cs

--- Bloxstrap/Enums/BootstrapperIcon.cs
+++ Bloxstrap/Enums/BootstrapperIcon.cs
@@ -4,8 +4,16 @@
     {
         [EnumName(StaticName = "Fishstrap")] //ermmmm it goes unused as of rn
         IconFishstrap,
-        [EnumName(StaticName = "Fishstrap")]
+        [EnumName(StaticName = "Fishstrap: Unlocked")]
         IconBloxstrap,
+        [EnumName(FromTranslation = "Fishstrap (Modern)")]
+        IconFishstrapModern,
+        [EnumName(FromTranslation = "Fishstrap (Classic)")]
+        IconFishstrapClassic,
+        [EnumName(FromTranslation = "Bloxstrap (Modern)")]
+        IconBloxstrapModern,
+        [EnumName(FromTranslation = "Bloxstrap (Classic)")]
+        IconBloxstrapClassic,
         [EnumName(StaticName = "2008")]
         Icon2008,
         [EnumName(StaticName = "2011")]
@@ -21,8 +29,6 @@
         [EnumName(StaticName = "2022")]
         Icon2022,
         [EnumName(FromTranslation = "Common.Custom")]
-        IconCustom,
-        [EnumName(StaticName = "Bloxstrap (Classic)")]
-        IconBloxstrapClassic
+        IconCustom
     }
 }
