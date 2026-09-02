f88add02536a8ce69900a98230dca81302f4df32
LudwigGaming77YT <43101660+LudwigGaming77YT@users.noreply.github.com>
2026-09-02 11:22:06 -0500

    Update App.xaml.cs

--- Bloxstrap/App.xaml.cs
+++ Bloxstrap/App.xaml.cs
@@ -14,13 +14,13 @@ namespace Bloxstrap
     public partial class App : Application
     {
 #if QA_BUILD
-        public const string ProjectName = "Fishstrap-QA";
+        public const string ProjectName = "Fishstrap-Unlocked-QA";
 #else
-        public const string ProjectName = "Fishstrap";
+        public const string ProjectName = "Fishstrap-Unlocked";
 #endif
-        public const string ProjectOwner = "returnrqt";
-        public const string ProjectRepository = "returnrqt/fishstrap";
-        public const string ProjectDownloadLink = "https://github.com/returnrqt/fishstrap/releases";
+        public const string ProjectOwner = "LudwigGaming77YT";
+        public const string ProjectRepository = "ludwiggaming77yt/fishstrap-unlocked";
+        public const string ProjectDownloadLink = "https://github.com/LudwigGaming77YT/fishstrap-unlocked/releases";
         public const string ProjectHelpLink = "https://github.com/bloxstraplabs/bloxstrap/wiki";
         public const string ProjectSupportLink = "https://github.com/returnrqt/fishstrap/issues/new";
         public const string ProjectRemoteDataLink = "https://config.fishstrap.app/v1/Data.json";
